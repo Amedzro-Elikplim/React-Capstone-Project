f4c7ec116a7dae354e7e3fe05015e819cb6ebaee
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-22 22:47:37 +0000

    Set page routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Card from './components/Card/Card';
+import { Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
+import Weather from './pages/Weather';
+import Pollution from './pages/Pollution';
+import WeatherMap from './pages/WeatherMap';
 
 function App() {
   return (
     <>
       <Nav />
-      <Card />
+      <Routes>
+        <Route path="/" element={<Weather />} />
+        <Route path="/pollution" element={<Pollution />} />
+        <Route path="/map" element={<WeatherMap />} />
+      </Routes>
     </>
   );
 }
