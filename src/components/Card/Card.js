73af23fa80322623f611d0b9ca2a904e28f40537
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-22 18:58:06 +0000

    Insert vertical bar

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -11,19 +11,22 @@ import {
 function Card() {
   return (
     <div className="card m-3">
-      <div className="card-body">
-        <div className="d-flex align-items-center">
-          <h3 className="card-title">Ghana, </h3>
-          <p className="card-subtitle">West Africa</p>
-        </div>
+      <div className="card-body d-flex justify-content-center card-body-container">
+        <div>
+          <div className="d-flex align-items-center">
+            <h3 className="card-title">Ghana, </h3>
+            <p className="card-subtitle">West Africa</p>
+          </div>
 
-        <div className="d-flex icon-container">
-          <FiCloudLightning />
-          <FiCloudDrizzle />
-          <FiSun />
-          <FiCloud />
-          <FiSunrise />
+          <div className="d-flex icon-container">
+            <FiCloudLightning />
+            <FiCloudDrizzle />
+            <FiSun />
+            <FiCloud />
+            <FiSunrise />
+          </div>
         </div>
+        <div className="info">side bar</div>
       </div>
     </div>
   );
