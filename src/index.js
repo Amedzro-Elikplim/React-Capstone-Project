545305ba767c7e2ffde8ca57eb3e98bb48ee900d
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-22 22:47:23 +0000

    Wrap root component in hash router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/ConfigureStore';
 import './index.css';
@@ -11,7 +12,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
     ,
   </Provider>,
 );
