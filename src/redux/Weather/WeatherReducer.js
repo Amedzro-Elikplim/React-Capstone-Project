f5de1e751095c43ae6087af146091ecabf674c1c
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-22 22:09:43 +0000

    Remove unused import

--- src/redux/Weather/WeatherReducer.js
+++ src/redux/Weather/WeatherReducer.js
@@ -1,13 +1,10 @@
-import { act } from 'react-dom/test-utils';
-import { DISPLAY } from './ActionCreators';
-
 const initialState = [
   { country: 'Ghana', region: 'West Africa' },
   { country: 'Ghana', region: 'West Africa' },
   { country: 'Ghana', region: 'West Africa' },
 ];
 
-export const reducer = (state = initialState, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'DISPLAY_COUNTRIES':
       return initialState;
@@ -15,3 +12,5 @@ export const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+export default reducer;
