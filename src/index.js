a882d433214a7631a7e383957151e4d3ee30b39b
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-22 22:10:54 +0000

    Wrap root component with redux provider

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { Provider } from 'react-redux';
+import store from './redux/ConfigureStore';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+    ,
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
