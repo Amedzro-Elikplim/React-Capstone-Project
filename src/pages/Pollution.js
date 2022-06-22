3083037a7f6ad9873bf996a6eacc360db160e30d
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-22 21:13:48 +0000

    Fix linter error

---
+++ src/pages/Pollution.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Pollution() {
+  return (
+    <div>
+      Pollution
+    </div>
+  );
+}
+
+export default Pollution;
---
+++ src/pages/WeatherMap.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WeatherMap() {
+  return (
+    <div>
+      Weather maps
+    </div>
+  );
+}
+
+export default WeatherMap;
