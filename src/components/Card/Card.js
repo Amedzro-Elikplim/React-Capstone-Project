d9aac87544e1140844ec867ecb0578775f1ddb85
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-22 18:45:43 +0000

    Add weather icons to card

---
+++ src/components/Card/Card.js
@@ -0,0 +1,32 @@
+import './card.css';
+import React from 'react';
+import {
+  FiCloudLightning,
+  FiCloudDrizzle,
+  FiSun,
+  FiCloud,
+  FiSunrise,
+} from 'react-icons/fi';
+
+function Card() {
+  return (
+    <div className="card m-3">
+      <div className="card-body">
+        <div className="d-flex align-items-center">
+          <h3 className="card-title">Ghana, </h3>
+          <p className="card-subtitle">West Africa</p>
+        </div>
+
+        <div className="d-flex icon-container">
+          <FiCloudLightning />
+          <FiCloudDrizzle />
+          <FiSun />
+          <FiCloud />
+          <FiSunrise />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
