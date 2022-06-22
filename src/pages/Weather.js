f5a787b9bfe0612947cb6a588d9412da77458e8c
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-22 23:30:44 +0000

    Pass props to card component

--- src/pages/Weather.js
+++ src/pages/Weather.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Card from '../components/Card/Card';
 
 function Weather() {
+  const countries = useSelector((state) => state.weather);
+
   return (
-    <div>
-      Weather page
-    </div>
+    <>
+      {countries.map((country) => (
+        <Card
+          key={country.id}
+          country={country.country}
+          region={country.region}
+        />
+      ))}
+    </>
   );
 }
 
