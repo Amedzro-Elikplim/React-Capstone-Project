80beff681161e8cc6978900d1acebb012663f1d1
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-22 18:33:05 +0000

    Add card title and subtitle

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 function Card() {
-    return (
-        <div className="card">
-            
+  return (
+    <div className="card m-3">
+      <div className="card-body">
+        <div className="d-flex align-items-center">
+          <h3 className="card-title">Ghana, </h3>
+          <p className="card-subtitle">West Africa</p>
         </div>
-    );
+
+        <div>icons</div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
