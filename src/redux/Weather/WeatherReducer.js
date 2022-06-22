3d2509f81363b56812b0ba858e1088c15e309c7b
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-22 23:41:07 +0000

    Update countries in initial state

--- src/redux/Weather/WeatherReducer.js
+++ src/redux/Weather/WeatherReducer.js
@@ -1,7 +1,9 @@
 const initialState = [
   { id: 1, country: 'Ghana', region: 'West Africa' },
-  { id: 2, country: 'Ghana', region: 'West Africa' },
-  { id: 3, country: 'Ghana', region: 'West Africa' },
+  { id: 2, country: 'Nigeria', region: 'West Africa' },
+  { id: 3, country: 'UK', region: 'Europe' },
+  { id: 4, country: 'New your', region: 'USA' },
+  { id: 5, country: 'Germany', region: 'Europe' },
 ];
 
 const reducer = (state = initialState, action) => {
