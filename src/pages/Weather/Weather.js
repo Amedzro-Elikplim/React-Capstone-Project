db186b72e8a43ca5a58b85e6898cab8ca71e9890
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-23 18:00:39 +0000

    Add handler for navigation

--- src/pages/Weather/Weather.js
+++ src/pages/Weather/Weather.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import Card from '../../components/Card/Card';
 
 function Weather() {
   const countries = useSelector((state) => state.weather);
+  const navigate = useNavigate();
+
+  function navigateToDetails() {
+    console.log('hello');
+    navigate('/weatherdetails');
+  }
 
   return (
     <>
@@ -12,6 +19,8 @@ function Weather() {
           key={country.id}
           country={country.country}
           region={country.region}
+          onClick={() => navigateToDetails()}
+          onKeyDown={() => navigateToDetails()}
         />
       ))}
     </>
