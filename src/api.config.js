febb8ac65be90594c62c725ef0caefffe954ec95
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-24 17:03:27 +0000

    Add api key and weather api

---
+++ src/api.config.js
@@ -0,0 +1,4 @@
+const APIKEY = '87bb6e69e576992d5d10fc2ec170493b';
+const WEATHER_API = (latitude, longitude) => `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIKEY}`;
+
+export default WEATHER_API;
