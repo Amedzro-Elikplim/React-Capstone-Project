091eba647f9e8218c748157ae323894cd4f2adfc
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-24 18:49:16 +0000

    Dispatch thunk function

--- src/pages/Weather/WeatherDetails.js
+++ src/pages/Weather/WeatherDetails.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
+import { FETCHTHUNK } from '../../redux/Weather/ActionCreators';
 
 function WeatherDetails() {
   const location = useLocation();
-  console.log(location.state);
+  const dispatch = useDispatch();
+  const id = location.state;
 
+  useEffect(() => {
+    dispatch(FETCHTHUNK(id));
+  }, []);
+
+  const details = useSelector((state) => state.weather);
+  console.log(details);
   return (
     <div>
       <h1>Details</h1>
