dede2bd316ce52c80865bfe1ce583789ff04e86d
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-24 23:32:59 +0000

    Refactor code based on linter suggestion

--- src/redux/Weather/WeatherReducer.js
+++ src/redux/Weather/WeatherReducer.js
@@ -1,5 +1,7 @@
 const initialState = [
-  { id: 1, country: 'Ghana', region: 'West Africa', latitude: 5.5571096, longitude: -0.2012376 },
+  {
+    id: 1, country: 'Ghana', region: 'West Africa', latitude: 5.5571096, longitude: -0.2012376,
+  },
   { id: 2, country: 'Nigeria', region: 'West Africa' },
   { id: 3, country: 'UK', region: 'Europe' },
   { id: 4, country: 'New your', region: 'USA' },
@@ -10,7 +12,7 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_WEATHER':
       return action.payload;
-    
+
     default:
       return state;
   }
