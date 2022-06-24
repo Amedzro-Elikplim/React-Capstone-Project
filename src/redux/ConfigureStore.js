a41031ba2fa89f9c8b15e198065924c5ad6b0266
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-24 23:34:24 +0000

    Add country reducer

--- src/redux/ConfigureStore.js
+++ src/redux/ConfigureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import weather from './Weather/WeatherReducer';
+import countries from './Weather/CountryReducer';
 
 const store = configureStore({
   reducer: {
     weather,
+    countries,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
