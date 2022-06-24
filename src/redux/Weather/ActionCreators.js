973a7de6e0006c4aab277d7dcbfba178c3261d41
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-24 17:03:03 +0000

    Create thunk to fetch weather data

--- src/redux/Weather/ActionCreators.js
+++ src/redux/Weather/ActionCreators.js
@@ -1,4 +1,14 @@
-import { DISPLAY_COUNTRIES, FETCH_WEATHER } from './Actions';
+import { DISPLAY_COUNTRIES } from './Actions';
+import WEATHER_API from '../../api.config';
 
 export const DISPLAY = () => ({ type: DISPLAY_COUNTRIES });
-export const FETCH = (id) => ({ type: FETCH_WEATHER, payload: id });
+// export const FETCH = (id) => ({ type: FETCH_WEATHER, payload: id });
+
+export const FETCH = () => async (lat, log) => {
+    try {
+        const weather = await fetch(WEATHER_API(lat, log));
+        console.log(weather);
+    } catch (error) {
+        throw new Error(error);
+    }
+};
