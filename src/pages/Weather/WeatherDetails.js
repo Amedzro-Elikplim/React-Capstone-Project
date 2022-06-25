10533bfdb2bddad7fd5197f48b003c42da76b4be
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-25 16:45:02 +0000

    Display weather details

--- src/pages/Weather/WeatherDetails.js
+++ src/pages/Weather/WeatherDetails.js
@@ -21,65 +21,67 @@ function WeatherDetails() {
   const details = useSelector((state) => state.weather);
 
   return (
-    <div className="card p-2 m-2">
-      <div className="d-flex align-items-center gap-4">
-        <div className="d-flex gap-1 align-items-center justify-content-center">
-          <h1>
-            {details.name}
-            ,
-          </h1>
-          {/* <h2 className="weather-type">{main.main}</h2> */}
+    <div className="container-fluid container-parent d-flex justify-content-center align-items-center">
+      <div className="card card-container p-2 m-2">
+        <div className="d-flex align-items-center gap-4">
+          <div className="d-flex gap-1 align-items-center justify-content-center">
+            <h1>
+              {details.name}
+              ,
+            </h1>
+            {/* <h2 className="weather-type">{main.main}</h2> */}
+          </div>
+          <FiCloud />
+          <FiCloudDrizzle />
+          <FiSun />
         </div>
-        <FiCloud />
-        <FiCloudDrizzle />
-        <FiSun />
-      </div>
 
-      <div className="d-flex gap-2">
-        <div>
-          <p>latitude</p>
-          <p className="details">{ details.lat }</p>
+        <div className="d-flex gap-2">
+          <div>
+            <p>latitude</p>
+            <p className="details">{details.lat}</p>
+          </div>
+          <div>
+            <p>longitude</p>
+            <p className="details">{details.log}</p>
+          </div>
+          <div>
+            <p>temp</p>
+            <p className="details">{details.temp}</p>
+          </div>
+          <div>
+            <p>humid</p>
+            <p className="details">{details.humidity}</p>
+          </div>
+          <div>
+            <p>pressure</p>
+            <p className="details">{details.pressure}</p>
+          </div>
         </div>
-        <div>
-          <p>longitude</p>
-          <p className="details">{ details.log }</p>
-        </div>
-        <div>
-          <p>temp</p>
-          <p className="details">{ details.temp }</p>
-        </div>
-        <div>
-          <p>humid</p>
-          <p className="details">{ details.humidity }</p>
-        </div>
-        <div>
-          <p>pressure</p>
-          <p className="details">{ details.pressure }</p>
-        </div>
-      </div>
 
-      <div className="d-flex gap-2">
-        <div>
-          <p>wind speed</p>
-          <p className="details">{ details.wind_speed }</p>
+        <div className="d-flex gap-2">
+          <div>
+            <p>wind speed</p>
+            <p className="details">{details.wind_speed}</p>
+          </div>
+          <div>
+            <p>sunrise</p>
+            <p className="details">{details.sunrise}</p>
+          </div>
+          <div>
+            <p>sunset</p>
+            <p className="details">{details.sunset}</p>
+          </div>
+          <div>
+            <p>feels_like</p>
+            <p className="details">{details.humidity}</p>
+          </div>
         </div>
-        <div>
-          <p>sunrise</p>
-          <p className="details">{ details.sunrise }</p>
-        </div>
-        <div>
-          <p>sunset</p>
-          <p className="details">{ details.sunset }</p>
-        </div>
-        <div>
-          <p>feels_like</p>
-          <p className="details">{ details.humidity }</p>
-        </div>
-      </div>
 
-      <div className="d-flex gap-3 align-items-center">
-        <h3>Description</h3>
-        {details.description}
+        <div className="d-flex gap-3 align-items-center">
+          <h3>Description</h3>
+          {details.description}
+        </div>
       </div>
     </div>
   );
