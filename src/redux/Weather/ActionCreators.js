67c6c938f71cf787d2bc2462f4908405987fdbb4
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-25 20:17:49 +0000

    Refactor api call function

--- src/redux/Weather/ActionCreators.js
+++ src/redux/Weather/ActionCreators.js
@@ -4,13 +4,19 @@ import WEATHER_API from '../../api.config';
 export const FETCH = (data) => ({ type: FETCH_WEATHER, payload: data });
 export const DISPLAY = () => ({ type: DISPLAY_COUNTRIES, payload: null });
 
+export const fetchData = async (latitude, longitude) => {
+  const weather = await fetch(WEATHER_API(latitude, longitude));
+  const data = await weather.json();
+
+  return data;
+};
+
 export const FETCHTHUNK = (id) => async (dispatch, getState) => {
   try {
     const country = getState().countries.filter((item) => item.id === id);
     const { latitude, longitude } = country[0];
 
-    const weather = await fetch(WEATHER_API(latitude, longitude));
-    const data = await weather.json();
+    const data = await fetchData(latitude, longitude);
 
     const dataObj = {
       name: data.name,
