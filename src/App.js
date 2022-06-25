37c8792e7a69705135c7ed0a42cae3ea64b96722
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-25 18:46:30 +0000

    Remove unused routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Weather from './pages/Weather/Weather';
-import Pollution from './pages/Pollution';
-import WeatherMap from './pages/WeatherMap';
 import WeatherDetails from './pages/Weather/WeatherDetails';
 
 function App() {
@@ -11,8 +9,6 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Weather />} />
-        <Route path="/pollution" element={<Pollution />} />
-        <Route path="/map" element={<WeatherMap />} />
         <Route path="/weatherdetails" element={<WeatherDetails />} />
       </Routes>
     </>
